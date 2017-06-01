d319a1635ec2f10aca78b0dfc643516ba3e164b4
vineelas1214 <vineela.s1214@gmail.com>
2017-06-01 13:20:44 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import EasyABC from './EasyABC';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Easy ABC</h1>
+        </div>
+        <EasyABC />
+      </div>
+    );
+  }
+}
+
+export default App;
